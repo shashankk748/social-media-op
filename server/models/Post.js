401cf508d5025dab55be2534fd3cc76247512474
shashankk748 <shashank.m748@gmail.com>
2024-04-19 16:06:03 +0530

    changes to files

--- server/models/Post.js
+++ server/models/Post.js
@@ -1,73 +1,35 @@
-import Post from "../models/Post.js";
-import User from "../models/User.js";
+import mongoose from "mongoose";
 
-/* CREATE */
-export const createPost = async (req, res) => {
-  try {
-    const { userId, description, picturePath } = req.body;
-    const user = await User.findById(userId);
-    const newPost = new Post({
-      userId,
-      firstName: user.firstName,
-      lastName: user.lastName,
-      location: user.location,
-      description,
-      userPicturePath: user.picturePath,
-      picturePath,
-      likes: {},
-      comments: [],
-    });
-    await newPost.save();
+const postSchema = mongoose.Schema(
+  {
+    userId: {
+      type: String,
+      required: true,
+    },
+    firstName: {
+      type: String,
+      required: true,
+    },
+    lastName: {
+      type: String,
+      required: true,
+    },
+    location: String,
+    description: String,
+    picturePath: String,
+    userPicturePath: String,
+    likes: {
+      type: Map,
+      of: Boolean,
+    },
+    comments: {
+      type: Array,
+      default: [],
+    },
+  },
+  { timestamps: true }
+);
 
-    const post = await Post.find();
-    res.status(201).json(post);
-  } catch (err) {
-    res.status(409).json({ message: err.message });
-  }
-};
+const Post = mongoose.model("Post", postSchema);
 
-/* READ */
-export const getFeedPosts = async (req, res) => {
-  try {
-    const post = await Post.find();
-    res.status(200).json(post);
-  } catch (err) {
-    res.status(404).json({ message: err.message });
-  }
-};
-
-export const getUserPosts = async (req, res) => {
-  try {
-    const { userId } = req.params;
-    const post = await Post.find({ userId });
-    res.status(200).json(post);
-  } catch (err) {
-    res.status(404).json({ message: err.message });
-  }
-};
-
-/* UPDATE */
-export const likePost = async (req, res) => {
-  try {
-    const { id } = req.params;
-    const { userId } = req.body;
-    const post = await Post.findById(id);
-    const isLiked = post.likes.get(userId);
-
-    if (isLiked) {
-      post.likes.delete(userId);
-    } else {
-      post.likes.set(userId, true);
-    }
-
-    const updatedPost = await Post.findByIdAndUpdate(
-      id,
-      { likes: post.likes },
-      { new: true }
-    );
-
-    res.status(200).json(updatedPost);
-  } catch (err) {
-    res.status(404).json({ message: err.message });
-  }
-};
+export default Post;
--- server/models/User.js
+++ server/models/User.js
@@ -1,48 +1,45 @@
 import mongoose from "mongoose";
 
-
-const userSchema=new mongoose.Schema(
-    {
-        firstName:{
-            type:String,
-            required:true,
-            min:2,
-            max:50,
-        },
-        lastName:{
-            type:String,
-            required:true,
-            min:2,
-            max:50,
-        },
-        email:{
-            type:String,
-            required:true,
-            max:50,
-            unique:true,
-        },
-        password:{
-            type: String,
-            required:true,
-            min:5
-        },
-        picturePath:{
-            type:String,
-            default:"",
-        },
-        friends:{
-            type: Array,
-            default:[],
-        },
-        location: String,
-        occupation:String,
-        viewedProfile:Number,
-        impressions:Number,
-
-
-        
+const UserSchema = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+      min: 2,
+      max: 50,
     },
-{timestamps:true}
+    lastName: {
+      type: String,
+      required: true,
+      min: 2,
+      max: 50,
+    },
+    email: {
+      type: String,
+      required: true,
+      max: 50,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      min: 5,
+    },
+    picturePath: {
+      type: String,
+      default: "",
+    },
+    friends: {
+      type: Array,
+      default: [],
+    },
+    location: String,
+    occupation: String,
+    viewedProfile: Number,
+    impressions: Number,
+  },
+  { timestamps: true }
 );
-const User=mongoose.model("User",userSchema);
+
+const User = mongoose.model("User", UserSchema);
 export default User;
